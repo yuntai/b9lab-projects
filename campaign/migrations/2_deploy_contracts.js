8f420766d4ccfcd0ecb1a490b154c5a49424cea2
Yuntai Kyong <yuntai.kyong@gmail.com>
2017-08-12 21:49:59 +0900

    add constructor parameters in deploy()

--- campaign/migrations/2_deploy_contracts.js
+++ campaign/migrations/2_deploy_contracts.js
@@ -1,5 +1,6 @@
 var Campaign = artifacts.require("./Campaign.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Campaign);
+  console.log(web3.eth.accounts);
+  deployer.deploy(Campaign, 100, web3.toBigNumber(10000000));
 };
