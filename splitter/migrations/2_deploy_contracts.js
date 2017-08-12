6cf15986f64b9e4a7fae7d4531b5fa85b8c05904
Yuntai Kyong <yuntai.kyong@gmail.com>
2017-08-12 22:09:08 +0900

    add constructor parameter

--- splitter/migrations/2_deploy_contracts.js
+++ splitter/migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 var Splitter = artifacts.require("./Splitter.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Splitter);
+  var bobAddress = web3.eth.accounts[1];
+  var carolAddress = web3.eth.accounts[2];
+  deployer.deploy(Splitter, bobAddress, carolAddress);
 };
